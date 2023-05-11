dbdd3da3931fb13ee7f4e93259bfd40c9a4256c9
ngecu <devngecu@gmail.com>
2023-05-11 11:10:51 +0300

    gender distribution

--- src/screens/AnayticsScreen.js
+++ src/screens/AnayticsScreen.js
@@ -13,7 +13,7 @@ import SidebarComponent from '../components/SidebarComponent'
 import DashboardHeadComponent from '../components/DashboardHeader'
 import axios from 'axios'
 import DemoPie from '../components/PiechartComponent'
-import DemoLiquid from '../components/DemoLiquidComponent'
+import DemoBar from '../components/BarChatComponent'
 const AnalyticsScreen = ({ location, history }) => {
  
 
@@ -28,14 +28,71 @@ const AnalyticsScreen = ({ location, history }) => {
   const { loading_articles,articles } = articleListByUser
 
   const [genders,setGenders] = useState([])
+  const [genderD,setGenderD] = useState([])
+
   const [percentt,setPercenttt]= useState(0)
   const [loading,setLoading]= useState(true)
 
   const getGender = async () =>{
  const {data} = await axios.get('https://text-image-backend.onrender.com/api/data/gender')
  if (data) {
-  setGenders([{type:"males",value:data.males.length},{type:"females",value:data.females.length}])
   console.log("data is",data)
+
+
+
+
+
+  const age_groups = ["21-25", "16-20", "11-15", "6-10", "26-30", "1-5"];
+
+const countsByAgeGroup = {};
+
+age_groups.forEach(ageGroup => {
+  countsByAgeGroup[ageGroup] = {
+    Boys: 0,
+    Girls: 0
+  };
+});
+
+Object.values(data).forEach(arr => {
+  arr.forEach(obj => {
+    const { Age, Gender } = obj;
+    if (age_groups.includes(Age)) {
+      if (Gender === "Boy") {
+        countsByAgeGroup[Age].Boys++;
+      } else if (Gender === "Girl") {
+        countsByAgeGroup[Age].Girls++;
+      }
+    }
+  });
+});
+
+
+
+console.log("Counts by Age Group:", countsByAgeGroup);
+
+const x=[]
+Object.entries(countsByAgeGroup).forEach(([ageGroup, counts]) => {
+  const { Boys, Girls } = counts;
+
+  x.push(
+    {
+      label: ageGroup,
+      type: "Boys",
+      value: Boys
+    },
+    {
+      label: ageGroup,
+      type: "Girls",
+      value: Girls
+    }
+  );
+});
+
+console.log(x);
+
+setGenders(x)
+
+
   return true
  }
  else{
@@ -44,23 +101,36 @@ const AnalyticsScreen = ({ location, history }) => {
 
 
   }
-let percent = 0
-  const getSuccessfullGraduates = async () =>{
-    const {data} = await axios.get('https://text-image-backend.onrender.com/api/data/graduates')
-    
-    percent = (parseInt(data.successfull_graduates.length)/parseInt(data.results.length)) * 100
-console.log("percentage is ",percent)
-    setPercenttt(percent)
-    return true
-     }
+
+  const getGenderDistribution = async () => {
+    const {data} = await axios.get('https://text-image-backend.onrender.com/api/data/genderDistribution')
+    if (data) {
+     console.log("data is",data)
+
+     const mens_length= data.men
+     const women_length= data.women
+      const x = [
+     {
+      type: 'Men',
+      value: mens_length,
+    },
+    {
+      type: 'Women',
+      value: women_length,
+    }
+  ]
+  setGenderD(x)
+
+    }
+  }
 
      const call_both = async () => {
       setLoading(true);
-    
       const genderResult = await getGender();
-      const graduatesResult = await getSuccessfullGraduates();
+      const gDistr = await getGenderDistribution()
+     
     
-      if (genderResult && graduatesResult) {
+      if (genderResult && gDistr) {
         setLoading(false);
       }
     };
@@ -77,7 +147,7 @@ useEffect(()=>{
 
   return (
 <>
-{loading ? <Loader/> : (
+
 
 <Row>
 {/* <SidebarComponent /> */}
@@ -88,7 +158,7 @@ useEffect(()=>{
               <h5 className="card-title">Percentage of Graduated Students</h5>
               <p className="card-text">
              
-              <DemoLiquid percent={percentt} />
+              <DemoBar data={genders} />
 
               </p>
             </div>
@@ -99,14 +169,14 @@ useEffect(()=>{
             <div className="card-body">
               <h5 className="card-title">Gender Distribution</h5>
               <p className="card-text">
-              <DemoPie data={genders} />
+              <DemoPie data={genderD}  />
               </p>
             </div>
           </div>
         </Col>
         </Col>
       </Row>
-)}
+
 
 
 </>
